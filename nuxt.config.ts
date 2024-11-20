46dccc9661c803ff64320dd26b00436003d01235
Khokon-Chandra <khokonchandra4@gmail.com>
2024-11-21 01:43:36 +0600

    google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+    config: {
+
+    },
+    editorSupport: true
+  },
+
+
+  googleFonts: {
+    families: {
+      'Nunito Sans': {
+        wght: '200..900',
+        ital: '200..700',
+      },
+      Roboto: true,
+      Inter: [400, 700],
+      'Josefin+Sans': true,
+      Lato: [100, 300],
+      Raleway: {
+        wght: [100, 400],
+        ital: [100]
+      },
+      'Crimson Pro': {
+        wght: '200..900',
+        ital: '200..700',
+      }
+    },
+    download: false
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+
+
+    <div>
+        <h1 class="text-center p-4 text-gray-500 font-black text-4xl">Hi. I'm Vako.</h1>
+    </div>
+
+</template>
