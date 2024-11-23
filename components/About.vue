f27ed11735907b412b92e46e503675c208a7287b
Khokon-Chandra <khokonchandra4@gmail.com>
2024-11-23 19:34:57 +0600

    about

--- components/About.vue
+++ components/About.vue
@@ -8,7 +8,7 @@
                     class="inline-block font-black text-5xl text-gray-600 dark:text-gray-300 mb-4 bg-white/40 dark:bg-gray-800/40">
                     About Me</h1>
 
-                <p class="font-normal text-gray-700 dark:text-gray-200 bg-white/40 dark:bg-gray-900/60">
+                <p class="font-normal text-gray-800 dark:text-gray-200 bg-white/50 dark:bg-gray-900/60">
                     I'm a passionate web developer with extensive
                     experience in backend and full-stack development. I
                     build scalable, secure, and user-friendly web applications using Laravel, PHP, and modern
